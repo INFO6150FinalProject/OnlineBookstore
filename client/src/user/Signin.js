240e84d90a8808f8a02f71d51982cc40b8aee0eb
sihan zhao <zhao.sih@husky.neu.edu>
2020-04-15 08:50:24 -0400

    layout

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate,isAuthenticated} from "../auth";
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: "rachit@gmail.com",
-        password: "abcd9",
+        email: "",
+        password: "",
         error: "",
         loading: false,
         redirectToReferrer: false
