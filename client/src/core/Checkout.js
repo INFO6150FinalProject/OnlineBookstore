a4164429f5053b2c410c58c43e90b69344f29e9d
sihan zhao <zhao.sih@husky.neu.edu>
2020-04-15 09:46:27 -0400

    client braintree

--- client/src/core/Checkout.js
+++ client/src/core/Checkout.js
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from "react";
-import Layout from "./Layout";
-import { getProducts } from "./apiCore";
+import { getProducts, getBraintreeClientToken } from "./apiCore";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
 
 const Checkout = ({ products }) => {
+    const [data, setData] = useState({
+        success: false,
+        clientToken: null,
+        error: "",
+        instance: {},
+        address: ""
+    });
+
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
+
+    const getToken = (userId, token) => {
+        getBraintreeClientToken(userId, token).then(data => {
+            if (data.error) {
+                setData({ ...data, error: data.error });
+            } else {
+                setData({ ...data, clientToken: data.clientToken });
+            }
+        });
+    };
+
+    useEffect(() => {
+        getToken(userId, token);
+    }, []);
+
     const getTotal = () => {
         return products.reduce((currentValue, nextValue) => {
             return currentValue + nextValue.count * nextValue.price;
@@ -14,7 +39,7 @@ const Checkout = ({ products }) => {
 
     const showCheckout = () => {
         return isAuthenticated() ? (
-            <button className="btn btn-success">Checkout</button>
+            <div>{showDropIn()}</div>
         ) : (
             <Link to="/signin">
                 <button className="btn btn-primary">Sign in to checkout</button>
@@ -22,10 +47,60 @@ const Checkout = ({ products }) => {
         );
     };
 
+    const buy = () => {
+        // send the nonce to your server
+        // nonce = data.instance.requestPaymentMethod()
+        let nonce;
+        let getNonce = data.instance
+            .requestPaymentMethod()
+            .then(data => {
+                console.log(data);
+                nonce = data.nonce;
+                // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
+                // and also total to be charged
+                console.log(
+                    "send nonce and total to process: ",
+                    nonce,
+                    getTotal(products)
+                );
+            })
+            .catch(error => {
+                console.log("dropin error: ", error);
+                setData({ ...data, error: error.message });
+            });
+    };
+
+    const showDropIn = () => (
+        <div onBlur={() => setData({ ...data, error: "" })}>
+            {data.clientToken !== null && products.length > 0 ? (
+                <div>
+                    <DropIn
+                        options={{
+                            authorization: data.clientToken
+                        }}
+                        onInstance={instance => (data.instance = instance)}
+                    />
+                    <button onClick={buy} className="btn btn-success">
+                        Continue to Checkout
+                    </button>
+                </div>
+            ) : null}
+        </div>
+    );
+
+    const showError = error => (
+        <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+        >
+            {error}
+        </div>
+    );
+
     return (
         <div>
             <h2>Total: ${getTotal()}</h2>
-
+            {showError(data.error)}
             {showCheckout()}
         </div>
     );
--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import Layout from "../core/Layout";
 import { signin, authenticate, isAuthenticated } from "../auth";
